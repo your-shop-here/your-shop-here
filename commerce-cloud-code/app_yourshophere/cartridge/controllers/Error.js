4834828a8660e786f2a7ea6d0d9c372291fd72dc
Holger Nestmann <hnestmann@salesforce.com>
2025-04-11 13:21:11 +0200

    Update Error.js

--- commerce-cloud-code/app_yourshophere/cartridge/controllers/Error.js
+++ commerce-cloud-code/app_yourshophere/cartridge/controllers/Error.js
@@ -1,6 +1,6 @@
 /**
  * The Error controller doesn't use server.js (express-style, sfra-style)
- * This is to ensure we show proper errors, even when the express-style framework faces an error
+ * This is to ensure we show a proper error-page, even if the express-style framework faces an error
  */
 
 const system = require('dw/system/System');
