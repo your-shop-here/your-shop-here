077d5168477976c4acae2258458d9bb903c6fe36
Danny Gehl <dgehl@salesforce.com>
2025-05-07 14:58:28 +0200

    Welcome back message configurable in Page Designer

--- commerce-cloud-code/app_yourshophere/cartridge/partials/account/welcomeBack.js
+++ commerce-cloud-code/app_yourshophere/cartridge/partials/account/welcomeBack.js
@@ -8,17 +8,29 @@
 const createModel = (params) => {
     const Resource = require('dw/web/Resource');
     const URLUtils = require('dw/web/URLUtils');
+    const StringUtils = require('dw/util/StringUtils');
     const customer = params && params.customer;
-    let firstName = '';
+    let firstName = ''; let lastName = '';
     if (customer && customer.profile && customer.profile.firstName) {
         firstName = customer.profile.firstName;
+    }
+    if (customer && customer.profile && customer.profile.lastName) {
+        lastName = customer.profile.lastName;
+    }
+    let message;
+    if (firstName) {
+        if (params.settings && params.settings.welcomeMessage) {
+            message = StringUtils.format(params.settings.welcomeMessage, firstName, lastName);
+        } else {
+            message = Resource.msgf('welcome.back.named', 'translations', null, firstName, lastName);
+        }
+    } else {
+        message = Resource.msg('welcome.back', 'translations', null);
     }
     return {
         firstName,
         isAuthenticated: customer && customer.authenticated,
-        message: firstName
-            ? Resource.msgf('welcome.back.named', 'translations', null, firstName)
-            : Resource.msg('welcome.back', 'translations', null),
+        message,
         settings: params && params.settings,
         logoutUrl: URLUtils.url('Login-Logout'),
         logoutText: Resource.msg('button.logout', 'translations', null),
