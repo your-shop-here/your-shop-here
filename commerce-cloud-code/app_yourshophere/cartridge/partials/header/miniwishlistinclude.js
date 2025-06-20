d27bd4a1b310c3953b6d95977a21f9e7e4e37904
Danny Gehl <dgehl@salesforce.com>
2025-06-20 10:12:37 +0200

    Better theme application and updated configuration

--- commerce-cloud-code/app_yourshophere/cartridge/partials/header/miniwishlistinclude.js
+++ commerce-cloud-code/app_yourshophere/cartridge/partials/header/miniwishlistinclude.js
@@ -4,9 +4,23 @@
  */
 function createModel() {
     const StringUtils = require('dw/util/StringUtils');
+    const ProductListMgr = require('dw/customer/ProductListMgr');
+    const ProductList = require('dw/customer/ProductList');
+
+    let count = session.privacy.wishlistCount;
+    if (count === null) {
+        const wishlist = ProductListMgr.getProductLists(customer, ProductList.TYPE_WISH_LIST).toArray().pop();
+        if (wishlist) {
+            session.privacy.wishlistCount = wishlist.productItems.length;
+            count = session.privacy.wishlistCount;
+        } else {
+            session.privacy.wishlistCount = 0;
+            count = 0;
+        }
+    }
     const model = {
         wishlistInfo: {
-            itemCount: StringUtils.formatNumber(session.privacy.wishlistCount || 0, '0.#'),
+            itemCount: StringUtils.formatNumber(count, '0.#'),
         },
     };
     return model;
