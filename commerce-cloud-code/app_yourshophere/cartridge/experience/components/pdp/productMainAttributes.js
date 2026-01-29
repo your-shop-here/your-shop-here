e4fe40c8a2399d561c811eee03d145e177a04034
SGD1953 <dgehl@salesforce.com>
2026-01-29 10:00:03 +0100

    Added swatch support for PDP, Fixed update bug on PDP (#90)
    * Added swatch support for PDP, Fixed update bug on PDP
    
    * Fixed color loss on size selection
    
    * Simplified code

--- commerce-cloud-code/app_yourshophere/cartridge/experience/components/pdp/productMainAttributes.js
+++ commerce-cloud-code/app_yourshophere/cartridge/experience/components/pdp/productMainAttributes.js
@@ -4,6 +4,15 @@
  * @param {dw.experience.ComponentScriptContext} context The component context
  * @returns {string} The template to be displayed
  */
-exports.render = function render() {
-    return require('*/api/partials').create('pdp/variationAttributes').html(request.custom.model.product);
+exports.render = function render(context) {
+    const product = request.custom.model && request.custom.model.product ? request.custom.model.product : null;
+    const swatchDimension = context.content && context.content.swatchDimension ? context.content.swatchDimension : '';
+    const swatchViewType = context.content && context.content.swatchViewType ? context.content.swatchViewType : '';
+    const swatchDISConfig = context.content && context.content.swatchDISConfig ? context.content.swatchDISConfig : '';
+    return require('*/api/partials').create('pdp/variationAttributes').html({
+        product,
+        swatchDimension,
+        swatchViewType,
+        swatchDISConfig,
+    });
 };
